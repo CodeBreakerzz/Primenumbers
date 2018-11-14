a5b8d6c32883d92c7d488f9b18f8a0437ee7b9de
Dunnb18 <dunnb18@students.ecu.edu>
2018-11-13 20:47:42 -0500

    my first push

---
+++ primeNumbers.java
@@ -0,0 +1,42 @@
+package codeBreakerzz;
+
+public class primeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	//Prime number
+	for(int n = 2;n<=1000; n++)
+	{
+		for(int j = 2; j<=n; j++) 
+		{	
+			
+		if(j==n) 
+		{
+			System.out.println(n);
+		}
+		if(n%j==0)
+		{
+		break;
+	}
+		}
+	}
+	}
+}
+	
+	
+		
+	
+	
+
+		
+	
+		
+	
+	
+
+		
+	
+	
+
+
